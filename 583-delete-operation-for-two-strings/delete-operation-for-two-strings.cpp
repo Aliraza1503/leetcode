2369c1a77b07ddbfbc10710313527e63ca028c26
Aliraza1503 <aliraza150305@gmail.com>
2026-07-17 00:58:10 +0530

    Time: 68 ms (5.01%) | Memory: 10.8 MB (99.52%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int n;
-    int m;
+    int n,m;
     int dp[501][501];
-    int solve(int i,int j,string&s1,string&s2){
-        if(i>=n||j>=m){ return 0;}
+    int solve(int i,int j,string &w1,string & w2){
+        if(i>=n||j>=m) return 0;
+        int take = 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int take =0;
-        if(s1[i]==s2[j]){
-            take = 1+solve(i+1,j+1,s1,s2);
+        if(w1[i]==w2[j]){
+            take = 1 + solve(i+1,j+1,w1,w2);
         }
-        int skipleft = solve(i+1,j,s1,s2);
-        int skipright = solve(i,j+1,s1,s2);
-        return dp[i][j]= max(take,max(skipleft,skipright));
+        int left = solve(i+1,j,w1,w2);
+        int right = solve(i,j+1,w1,w2);
+        return dp[i][j]= max(take,max(left,right));
     }
-    int minDistance(string word1, string word2) {
-        n = word1.size();
-        m = word2.size();
+    int minDistance(string w1, string w2) {
+        n = w1.size();
+        m = w2.size();
         memset(dp,-1,sizeof(dp));
-        int val=solve(0,0,word1,word2);
-        
-        return (n+m)-(2*val);
+        int val = solve(0,0,w1,w2);
+        return n+m-2*val;
     }
 };

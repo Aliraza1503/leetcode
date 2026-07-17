2f3fc240f298a84c93d25b21968688f788dc7108
Aliraza1503 <aliraza150305@gmail.com>
2026-07-17 10:30:11 +0530

    Time: 49 ms (11.61%) | Memory: 10.4 MB (93.77%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int n,m;
+    int n, m;
     int dp[501][501];
-    int solve(int i,int j,string&s1,string&s2){
-        if(i==n){
-            return m-j;
-        }
-        if(j==m){
-            return n-i;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int insert = 1e5;
-        int del = 1e5;
+    int solve(int i,int j,string &w1,string&w2){
+        if(j==m) return n-i;
+        if(i==n) return m-j;
+        int take = 1e5;
         int replace = 1e5;
-     
-        if(s1[i]==s2[j]){
-        return dp[i][j]= solve(i+1,j+1,s1,s2);
+        int del = 1e5;
+        int ins=1e5;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(w1[i]==w2[j]){
+           take= solve(i+1,j+1,w1,w2);
         }
-        else {
-            insert = 1 + solve(i,j+1,s1,s2);
-            del = 1 + solve(i+1,j,s1,s2);
-            replace = 1+solve(i+1,j+1,s1,s2);
+        else{
+         replace = 1 + solve(i+1,j+1,w1,w2);
+        del = 1 + solve(i+1,j,w1,w2);
+        ins = 1 + solve(i,j+1,w1,w2);
         }
-        return dp[i][j]= min(insert,min(del,replace));
+        return dp[i][j]= min(min(ins,replace),min(take,del));
     }
     int minDistance(string word1, string word2) {
         n = word1.size();
-        m = word2.size();
-        
-        // if(n>=m){
-        //      temp = word1;
-        // }
-        // else{
-        //     temp = word2;
-        // }
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,word1,word2);
+         m = word2.size();
+       memset(dp,-1,sizeof(dp));
+        return  solve(0,0,word1,word2);
     }
 };

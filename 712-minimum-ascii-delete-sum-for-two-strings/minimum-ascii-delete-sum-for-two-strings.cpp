6ce9c8a53742a90735b5273934c4e89ed15d4c57
Aliraza1503 <aliraza150305@gmail.com>
2026-07-17 10:57:00 +0530

    Time: 31 ms (35.49%) | Memory: 13.3 MB (90.04%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -3,36 +3,28 @@ public:
     int n,m;
     int dp[1001][1001];
     int solve(int i,int j,string&s1,string&s2){
-        if(i>=n){
-            int sum =0;
-            for(int k = j;k<m;k++){
-                sum+=s2[k];
-            }
-            return sum;
-        }
-          if(j>=m){
-            int sum =0;
-            for(int k = i;k<n;k++){
-                sum+=s1[k];
-            }
-            return sum;
-          }
-          if(dp[i][j]!=-1) return dp[i][j];
-        int equal = 1e5;
-        int skipleft = 1e5;
-        int skipright = 1e5;
+        if(i>=n||j>=m) return 0;
+        int take =0;
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-            equal = solve(i+1,j+1,s1,s2);
+            take = 2*s1[i]+solve(i+1,j+1,s1,s2);
         }
-        skipleft = s1[i]+solve(i+1,j,s1,s2);
-        skipright = s2[j]+solve(i,j+1,s1,s2);
-        return dp[i][j]= min(equal,min(skipleft,skipright));
-
-          }
+        int takeleft = solve(i+1,j,s1,s2);
+        int takeright = solve(i,j+1,s1,s2);
+        return dp[i][j]= max(take,max(takeleft,takeright));
+    }
     int minimumDeleteSum(string s1, string s2) {
         n = s1.size();
         m = s2.size();
+        int s1val=0;
+        int s2val=0;
+        for(auto it:s1){
+            s1val+=it;
+        }
+        for(auto it:s2){
+            s2val+=it;
+        }
         memset(dp,-1,sizeof(dp));
-        return solve(0,0,s1,s2);
+        return s1val+s2val-solve(0,0,s1,s2);
     }
 };

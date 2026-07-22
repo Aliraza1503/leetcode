3fc714c4e62554d69a97253683adade9dac55c24
Aliraza1503 <aliraza150305@gmail.com>
2026-07-23 01:28:01 +0530

    Time: 186 ms (20.94%) | Memory: 13.9 MB (41.91%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    bool solve(int i,int j,string&s){
+     if(i>=j) return true;
+     if(dp[i][j]!=-1) return dp[i][j];
+     if(s[i]==s[j]){
+       return dp[i][j]= solve(i+1,j-1,s);
+     }
+     return dp[i][j]= false;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int len = INT_MIN;
+        int sp =0;
+        memset(dp,-1,sizeof(dp));
+        for(int i =0;i<n;i++){
+            for(int j =i;j<n;j++){
+                if(solve(i,j,s)==true){
+                    if(j-i+1>len){
+                        len = j-i+1;
+                        sp=i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp,len);
+    }
+};

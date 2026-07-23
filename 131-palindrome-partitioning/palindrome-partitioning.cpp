f85050111283188807274163140c0c5eec67715a
Aliraza1503 <aliraza150305@gmail.com>
2026-07-23 14:32:43 +0530

    Time: 28 ms (92.73%) | Memory: 53.4 MB (83.77%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int n;
+    int dp[17][17];
     bool ispalindrome(int idx,int i,string&s){
         if(idx>=i){
             return true;
         }
+        if(dp[idx][i]!=-1) return dp[idx][i];
         if(s[i]==s[idx]){
-            return ispalindrome(idx+1,i-1,s);
+            return dp[idx][i]= ispalindrome(idx+1,i-1,s);
         }
-        return false;
+        return dp[idx][i]= false;
     }
     void backtrack(int idx,string&s,vector<string>&store,vector<vector<string>>&ans){
         if(idx==n){
@@ -27,6 +29,7 @@ public:
         vector<vector<string>> ans;
         vector<string> store;
         n = s.size();
+        memset(dp,-1,sizeof(dp));
         backtrack(0,s,store,ans);
         return ans;
     }

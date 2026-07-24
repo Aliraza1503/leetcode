8ceb2c6d20cbb60569268eee7c5ef72578ca98f2
Aliraza1503 <aliraza150305@gmail.com>
2026-07-24 20:41:23 +0530

    Time: 18 ms (75.47%) | Memory: 29.3 MB (54.29%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    typedef long long ll;
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<ll>> store(n,vector<ll>(m,LLONG_MAX));
+        priority_queue<pair<ll,pair<int,int>>,vector<pair<ll,pair<int,int>>>,greater<pair<ll,pair<int,int>>>> pq;
+        pq.push({0,{0,0}});
+        store[0][0]=0;
+        vector<int> row = {-1,0,1,0};
+        vector<int> col = {0,-1,0,1};
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int curtime = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
+            if(i==n-1&&j==m-1) {return curtime;}
+            for(int k = 0;k<4;k++){
+                int i1 = i+row[k];
+                int j1 = j+col[k];
+                if(i1>=0&&i1<n&&j1>=0&&j1<m){
+                    int wait =max(0,moveTime[i1][j1]-curtime);
+                    int fin = wait+curtime+1;
+                    if(store[i1][j1]>fin){
+                        store[i1][j1]=fin;
+                        pq.push({fin,{i1,j1}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};

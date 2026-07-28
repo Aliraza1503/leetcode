901ac8d56427e595c2ba12e454d6423dccbfad63
Aliraza1503 <aliraza150305@gmail.com>
2026-07-28 23:11:38 +0530

    Time: 230 ms (57.14%) | Memory: 108.6 MB (89.05%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
-         int n = grid.size();
-       int m = grid[0].size();
-       vector<vector<int>> store(n,vector<int>(m,INT_MAX));
-       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-       pq.push({0,{0,0}});
-      
-       store[0][0]=0;
-       vector<int> row={-1,0,1,0};
-       vector<int> col = {0,-1,0,1};
-       while(!pq.empty()){
-        auto it = pq.top();
-        pq.pop();
-        int cur = it.first;
-        int i = it.second.first;
-        int j = it.second.second;
-        if(i==n-1&&j==m-1) return cur;
-        for(int k =0;k<4;k++){
-            int i1 = i+row[k];
-            int j1 = j + col[k];
-            if(i1>=0&&i1<n&&j1>=0&&j1<m){
-                int val = cur+grid[i1][j1];
-                if(val<store[i1][j1]){
-                    store[i1][j1]=val;
-                    pq.push({val,{i1,j1}});
+        int n = grid.size();
+        int m = grid[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>> store(n,vector<int>(m,INT_MAX));
+        store[0][0]=0;
+        pq.push({0,{0,0}});
+        vector<int> row = {-1,0,1,0};
+        vector<int> col = {0,-1,0,1};
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int cur = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
+            if(i==n-1&&j==m-1) return cur;
+            for(int k=0;k<4;k++){
+                int p = i+row[k];
+                int q = j+col[k];
+                if(p>=0&&p<n&&q>=0&&q<m){
+                    if(grid[p][q]==0){
+                        if(store[p][q]>cur){
+                            store[p][q]=cur;
+                            pq.push({cur,{p,q}});
+                        }
+                    }
+                    else{
+                        if(store[p][q]>cur+1){
+                            store[p][q]=cur+1;
+                            pq.push({cur+1,{p,q}});
+                        }
+                    }
                 }
-               
             }
         }
-       }
-       return store[n-1][m-1];
+        return 0;
     }
 };

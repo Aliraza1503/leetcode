533748c595c4bcf9db6c5ef662a60d7087679a99
Aliraza1503 <aliraza150305@gmail.com>
2026-08-19 18:49:00 +0530

    Time: 7 ms (11.44%) | Memory: 21.4 MB (6.27%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,53 +1,33 @@
 class Solution {
 public:
-    int n;
-    vector<int> parent;
-    vector<int> rank;
-
-    int find(int x){
-            if(parent[x]==x) return x;
-            return parent[x]= find(parent[x]);
+    void dfs(int node,unordered_map<int,vector<int>>&adj,vector<int>&vis){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(it,adj,vis);
+            }
+        }
     }
-    int unio(int x,int y){
-        int x_parent=find(x);
-        int y_parent = find(y);
-        if(x_parent==y_parent) return 1;
-        if(rank[x_parent]>rank[y_parent]){
-            parent[y_parent]=x_parent;
-        }
-       else if(rank[x_parent]<rank[y_parent]){
-            parent[x_parent]=y_parent;
-        }
-        else{
-            parent[x_parent]=y_parent;
-            rank[y_parent]++;
-        }
-        return 0;
-    }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        unordered_map<int,vector<int>> mp;
-        int n = isConnected.size();
-        rank.resize(n,0);
-        parent.resize(n);
+    int findCircleNum(vector<vector<int>>& nums) {
+         unordered_map<int,vector<int>>adj;
+         int n = nums.size();
         for(int i =0;i<n;i++){
-            parent[i]=i;
-        }
-        for(int i =0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j&&isConnected[i][j]==1){
-                    mp[i].push_back(j);
+            for(int j =0;j<n;j++){
+                if(i!=j&&nums[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        int ans =n;
-       for(int i =0;i<n;i++){
-        for(auto it:mp[i]){
-            if(unio(it,i)==0){
-                ans--;
+        
+        vector<int> vis(n,0);
+        int ans =0;
+        for(int i =0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,adj,vis);
+                ans++;
             }
         }
-       }
-       return ans;
+        return ans;
     }
 };

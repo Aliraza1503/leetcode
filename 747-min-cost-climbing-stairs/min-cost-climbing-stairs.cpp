24750d31c1e3401000a890bfe49f6552a65ce451
Aliraza1503 <aliraza150305@gmail.com>
2026-08-21 01:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (75.38%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[1001];
+    int n;
+    int solve(int i,vector<int>&cost){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take1 = cost[i]+solve(i+1,cost);
+        int take2 = cost[i]+solve(i+2,cost);
+        return dp[i]= min(take1,take2);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        n = cost.size();
+        memset(dp,-1,sizeof(dp));
+        return min(solve(0,cost),solve(1,cost));
+    }
+};

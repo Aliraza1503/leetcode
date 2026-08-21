d411b924e210b19492be39436808c91b1c3a90fc
Aliraza1503 <aliraza150305@gmail.com>
2026-08-21 22:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (46.42%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int n;
-    int dp[101];
-    int solve(int i,string&s){
-         if(s[i]=='0') return 0;
-        if(i>=n-1) return 1;
-       
-        if(dp[i]!=-1) return dp[i];
-        int take1 = solve(i+1,s);
-        int take2 =0;
-        if(i<n-1&&(s[i]=='1'&&(s[i+1]>='0'&&s[i+1]<='9'))||(i<n-1&&(s[i]=='2'&&(s[i+1]>='0'&&s[i+1]<='6')))){
-            take2 = solve(i+2,s);
-        }
-        return dp[i]= take1+take2;
-
-        
-        
-    }
     int numDecodings(string s) {
     n = s.size();
-    if(s[0]=='0') return 0;
-    memset(dp,-1,sizeof(dp));
-    return solve(0,s);
+    // if(s[0]=='0') return 0;
+    // memset(dp,-1,sizeof(dp));
+    // return solve(0,s);
+    vector<int> dp(n+1);
+    dp[n]=1;
+    for(int i =n-1;i>=0;i--){
+        if(s[i]=='0'){
+            dp[i]=0;
+        }
+        else {
+            dp[i]=dp[i+1];
+            if(i<n-1&&(s[i]=='1'||(s[i]=='2'&&s[i+1]>='0'&&s[i+1]<='6'))){
+                dp[i]+=dp[i+2];
+            }
+        }
+    }
+    return dp[0];
     }
 };

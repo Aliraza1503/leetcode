a6ed9889a7a7ed4f9902f40c992031f26a819ef6
Aliraza1503 <aliraza150305@gmail.com>
2026-08-22 02:00:19 +0530

    Time: 27 ms (8.25%) | Memory: 21.3 MB (7.79%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int n;
+    unordered_set<string> st;
+    int dp[301];
+    bool solve(int idx,string&s){
+        if(idx==n){
+            return true;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        if(st.find(s)!=st.end()) return true;
+        for(int l =1;l<=n;l++){
+            string temp =s.substr(idx,l);
+            if(st.find(temp)!=st.end()&&solve(idx+l,s)) return true;
+        }
+        return dp[idx]= false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+       n = s.size();
+       for(auto it:wordDict){
+        st.insert(it);
+       }
+       memset(dp,-1,sizeof(dp));
+       return solve(0,s);
+    }
+};

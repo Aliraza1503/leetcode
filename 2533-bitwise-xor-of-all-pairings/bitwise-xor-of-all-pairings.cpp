037955464f1232b6f8a9b11e101ac01bf1f8bdcf
Aliraza1503 <aliraza150305@gmail.com>
2026-09-06 01:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 64.5 MB (10.21%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -4,28 +4,26 @@ public:
   
       int n1 = nums1.size();
       int n2 = nums2.size();
+      int val=0;
         if(n1%2==0&&n2%2==0) return 0;
-        if(n1%2==0) {
-            int val=0;
+       else if(n1%2==0) {
             for(auto it:nums1){
                 val^=it;
             }
-            return val;
         }
-         if(n2%2==0) {
-            int val=0;
+       else  if(n2%2==0) {
             for(auto it:nums2){
                 val^=it;
             }
-            return val;
         }
-        int val1=0;
+        else {
         for(auto it:nums1){
-            val1^=it;
+            val^=it;
         }
         for(auto it:nums2){
-            val1^=it;
+            val^=it;
         }
-        return val1;
+        }
+        return val;
     }
 };

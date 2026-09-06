562c164338f841d2566642fc966fe3da96c63d19
Aliraza1503 <aliraza150305@gmail.com>
2026-09-06 23:22:36 +0530

    Time: 9 ms (16.81%) | Memory: 19.7 MB (5.35%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-          unordered_map<int,vector<int>> mp;
-        vector<int> indeg(numCourses,0);
+    bool dfs(int i, unordered_map<int,vector<int>> &mp, vector<int> &vis,vector<int>&ans){
+        vis[i]=1;
+        for(auto it:mp[i]){
+            if(vis[it]==1) return true;
+            else if(vis[it]==0){
+                if(dfs(it,mp,vis,ans)) return true;
+            }
+        }
+        vis[i]=2;
+        ans.push_back(i);
+        return false;
+    }
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        unordered_map<int,vector<int>> mp;
         for(auto it:pre){
             mp[it[1]].push_back(it[0]);
-            indeg[it[0]]++;
         }
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-           if(indeg[i]==0) q.push(i);
-        }
+        vector<int> vis(n,0);
         vector<int> ans;
-        while(!q.empty()){
-            int val = q.front();
-            q.pop();
-            ans.push_back(val);
-            for(auto it:mp[val]){
-                indeg[it]--;
-                if(indeg[it]==0) q.push(it);
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,mp,vis,ans);
             }
         }
         vector<int> store;
-        return ans.size()==numCourses?ans:store;
+        reverse(ans.begin(),ans.end());
+        return ans.size()==n?ans:store;
     }
 };

6302bc7fa1501435f7b9adaf0e6d31f6e4240d8c
Aliraza1503 <aliraza150305@gmail.com>
2026-09-11 18:01:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (70.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -10,7 +10,21 @@ public:
         return dp[i][j]= right + down;
     }
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,m,n);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,0,m,n);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+       for(int i=n-1;i>=0;i--){
+        for(int j =m-1;j>=0;j--){
+            if(i==n-1&&j==m-1){
+                dp[n-1][m-1]=1;
+            }
+            else{
+                int right = dp[i+1][j];
+                int up = dp[i][j+1];
+                dp[i][j]=right+up;
+            }
+        }
+       }
+       return dp[0][0];
     }
 };

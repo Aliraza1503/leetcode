fdadcbaefac2333c76d3033968eba5e6abb356a6
Aliraza1503 <aliraza150305@gmail.com>
2026-09-14 00:28:35 +0530

    Time: 58 ms (91.21%) | Memory: 22 MB (53.08%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    int dp[201][10001];
+    bool solve(int i,int target,vector<int>&nums){
+        if(i>=n) return false;
+        if(target==0) return true;
+        if(target<0) return false;
+        if(dp[i][target]!=-1) return dp[i][target];
+      bool  take = solve(i+1,target-nums[i],nums);
+      if(take) return dp[i][target]= true;
+      bool ntake = solve(i+1,target,nums);
+      return dp[i][target]= ntake;
+
+    }
+    bool canPartition(vector<int>& nums) {
+        int val=0;
+        n = nums.size();
+        for(auto it:nums){
+            val+=it;
+        }
+        if(val%2!=0) return false;
+        int tar = val/2;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,tar,nums);
+    }
+};

2a1a2c9cd1032d7defc6f68465d12eeb977ae3b0
Aliraza1503 <aliraza150305@gmail.com>
2026-09-13 21:01:07 +0530

    Time: 103 ms (10.56%) | Memory: 149.9 MB (10.38%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+       //length of longest subarray and sum has to be k length must be greater than or equal 
+       // to 2 
+       unordered_map<int,int> mp;
+       mp[0]=-1;
+       int ans =0;
+       int val =0;
+       for(int i=0;i<nums.size();i++){
+        int it = nums[i];
+        val+=it;
+        int rem = val%k;
+        if(rem<0) rem+=k;
+        if(mp.find(rem)!=mp.end()){
+            ans = max(ans,i-mp[rem]);
+            // if(ans>=2) return true;
+        }
+       else{ 
+         mp[rem]=i;
+       }
+       }
+       return ans>=2;
+    }
+};

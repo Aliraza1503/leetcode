211f005e641f58c482a911e4677c8eafbbe4e2c3
Aliraza1503 <aliraza150305@gmail.com>
2026-09-20 18:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (68.55%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int gcd(int x,int y){
+        int ans =1;
+        for(int i=1;i<=min(x,y);i++){
+            if(x%i==0&&y%i==0){
+                ans = i;
+            }
+        }
+        return ans;
+    }
+    bool canMeasureWater(int x, int y, int target) {
+        //condition if x+y<target return false;
+        // find gcd of x and y and check is it divide the target or not
+        if(x+y<target) return false;
+        return target%gcd(x,y)==0;
+    }
+};

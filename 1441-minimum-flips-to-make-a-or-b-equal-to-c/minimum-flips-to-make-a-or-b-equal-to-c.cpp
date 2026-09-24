6d66ed6e3f5d6a534d154ba22edb5d69bcca87fb
Aliraza1503 <aliraza150305@gmail.com>
2026-09-25 00:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (92.94%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flip =0;
-        while(a!=0||b!=0||c!=0){
-            if((c&1)==1){
-                if(((b&1)==0)&&(a&1)==0) flip++;
-            }
-            else {
-                if((b&1)==1) flip++;
-                if((a&1)==1) flip++;
-            }
-            a>>=1;
-            b>>=1;
-            c>>=1;
+        /*
+          a 10 
+          b110
+          c101
+          // approach
+          leftshit^1==0?leftshift = 1:0;
+           check c 1 ab=0 and ab1 or else no check 
+           check c =0 
+           got the lotic right 
+        */
+        int ans =0;
+        for(int i=0;i<31;i++){
+                int cright = (c>>i)&1;
+                int bright = (b>>i)&1;
+                int aright = (a>>i)&1;
+                if(cright==0){
+                    if(aright==1&&bright==1){
+                        ans+=2;
+                    }
+                    else if(aright==1||bright==1){
+                        ans+=1;
+                    }
+                }
+                else{
+                    if((aright==0&&bright==0)){
+                        ans+=1;
+                    }
+                }
         }
-        return flip;
+        return ans;
     }
 };

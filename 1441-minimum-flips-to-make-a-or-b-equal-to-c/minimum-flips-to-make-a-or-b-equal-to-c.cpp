5d43fa696539770c6a938e6f41ca2834f6aed7ca
Aliraza1503 <aliraza150305@gmail.com>
2026-09-25 00:51:04 +0530

    Time: 3 ms (24.88%) | Memory: 7.8 MB (38.60%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -11,25 +11,41 @@ public:
            check c =0 
            got the lotic right 
         */
-        int ans =0;
-        for(int i=0;i<31;i++){
-                int cright = (c>>i)&1;
-                int bright = (b>>i)&1;
-                int aright = (a>>i)&1;
-                if(cright==0){
-                    if(aright==1&&bright==1){
-                        ans+=2;
-                    }
-                    else if(aright==1||bright==1){
-                        ans+=1;
-                    }
-                }
-                else{
-                    if((aright==0&&bright==0)){
-                        ans+=1;
-                    }
-                }
-        }
+        //method 1
+        // int ans =0;
+        // for(int i=0;i<31;i++){
+        //         int cright = (c>>i)&1;
+        //         int bright = (b>>i)&1;
+        //         int aright = (a>>i)&1;
+        //         if(cright==0){
+        //           ans+=bright+aright;
+        //         }
+        //         else{
+        //             if((aright==0&&bright==0)){
+        //                 ans+=1;
+        //             }
+        //         }
+        // }
+        // return ans;
+
+
+        //method 2 convert bit to string best thing i study today 
+         
+         int ans =0;
+         string fir = bitset<31>(a).to_string();
+         string sec = bitset<31>(b).to_string();
+         string thir = bitset<31>(c).to_string();
+
+
+         for(int i=0;i<31;i++){
+            if(thir[i]=='0'){
+                ans+=((fir[i]-'0')+(sec[i]-'0'));
+            }
+            else{
+                ans+= (fir[i]=='0'&&sec[i]=='0')?1:0;
+            }
+         }
         return ans;
+
     }
 };
